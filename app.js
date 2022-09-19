fe4600231d3d9b210776b01e24b7ee0c36ed0a65
gabnatvidad <113141305+gabnatvidad@users.noreply.github.com>
2022-09-19 16:19:53 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let name;
+name = 'Gab';
+console.log (name);
+const DOB = 'March 21';
+console.log(DOB);
+let integer = 1;
+let float = 2.566845;
+let numberInstring = 'twenty two';
+let string = ' some tesxt and numbers etc';
+let boolean = true;
+let boolean2 = false;
+boolean = false;
+boolean = boolean2;
+
+let test = integer < float;
+console.log(24 == 21)
